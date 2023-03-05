2c80afe245ac51c335360a85b45892a72eec6d8b
ahmed <acc.ahmedstudio@gmail.com>
2023-03-05 21:03:30 +0400

    create category

--- src/main/java/com/a7medelnoor/ecommerce_app/service/CategoryService.java
+++ src/main/java/com/a7medelnoor/ecommerce_app/service/CategoryService.java
@@ -1,12 +1,13 @@
 package com.a7medelnoor.ecommerce_app.service;
 
-import com.a7medelnoor.ecommerce_app.*;
 import com.a7medelnoor.ecommerce_app.model.Category;
 import com.a7medelnoor.ecommerce_app.repository.CategoryRepository;
+import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -15,12 +16,18 @@ public class CategoryService {
     public void createCategory(Category category){
        categoryRepository.save(category);
     }
+    public Category readCategory(String categoryName){
+        return categoryRepository.findByCategoryName(categoryName);
+    }
+    public Optional<Category> readCategory(Integer categoryId){
+        return categoryRepository.findById(categoryId);
+    }
     public List<Category> getListCategory(){
         return categoryRepository.findAll();
     }
 
-    public void updateCategory(int categoryId, Category updateCategory) {
-        Category category = categoryRepository.getById(categoryId);
+    public void editCategory(Integer categoryId, Category updateCategory) {
+        Category category = categoryRepository.findById(categoryId).get();
         category.setCategoryName(updateCategory.getCategoryName());
         category.setDescription(updateCategory.getDescription());
         category.setImageUrl(updateCategory.getImageUrl());
