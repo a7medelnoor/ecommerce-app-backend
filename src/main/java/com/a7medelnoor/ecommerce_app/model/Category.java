9cee8085e63a5546ed8bf124c81fd646c43e6c6c
ahmed <acc.ahmedstudio@gmail.com>
2023-03-05 21:02:38 +0400

    create category constructor and setter, getter

--- src/main/java/com/a7medelnoor/ecommerce_app/model/Category.java
+++ src/main/java/com/a7medelnoor/ecommerce_app/model/Category.java
@@ -1,24 +1,77 @@
 package com.a7medelnoor.ecommerce_app.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
-@Data
 @Entity
 @Table(name = "categories")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
+    private Integer id;
     @Column(name = "category_Name")
-    private String categoryName;
+    private @NotBlank String categoryName;
     @Column(name = "description")
 
-    private String description;
+    private @NotBlank String description;
     @Column(name = "image_url")
 
-    private String imageUrl;
+    private @NotBlank String imageUrl;
+
+    public Category(){}
+    public Category(@NotBlank String categoryName, @NotBlank String description) {
+        this.categoryName = categoryName;
+        this.description = description;
+    }
+    public Category(@NotBlank String categoryName, @NotBlank String description, @NotBlank String imageUrl) {
+        this.categoryName = categoryName;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", categoryName='" + categoryName + '\'' +
+                ", description='" + description + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
