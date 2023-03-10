9d92280737b03b44952e9d1ad0d463efeaf25e06
ahmed <acc.ahmedstudio@gmail.com>
2023-03-10 23:27:04 +0400

    create get user and authentication function

--- src/main/java/com/a7medelnoor/ecommerce_app/service/AuthenticationService.java
+++ src/main/java/com/a7medelnoor/ecommerce_app/service/AuthenticationService.java
@@ -1,10 +1,13 @@
 package com.a7medelnoor.ecommerce_app.service;
 
+import com.a7medelnoor.ecommerce_app.exceptions.AuthenticationFailedException;
 import com.a7medelnoor.ecommerce_app.model.AuthenticationToken;
 import com.a7medelnoor.ecommerce_app.model.User;
 import com.a7medelnoor.ecommerce_app.repository.TokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class AuthenticationService {
@@ -16,7 +19,28 @@ public class AuthenticationService {
       return  tokenRepository.findByUser(user);
     }
 
+    public User getUser(String token){
+        final AuthenticationToken authenticationToken = tokenRepository.findByToken(token);
+        if (Objects.isNull(authenticationToken)){
+            return null;
+        }
+      // authenticationToken is not null
+        return authenticationToken.getUser();
+    }
+
     public void saveConfirmationToken(AuthenticationToken authenticationToken) {
         tokenRepository.save(authenticationToken);
+    }
+    // authenticate the token
+
+    public void authenticate(String token){
+        // check if the token is valid or not
+        if (Objects.isNull(token)){
+            // throw an exceptiopn
+            throw  new AuthenticationFailedException("token not present");
+        }
+        if (Objects.isNull(getUser(token))){
+            throw new AuthenticationFailedException("token is not valid");
+        }
     }
 }
